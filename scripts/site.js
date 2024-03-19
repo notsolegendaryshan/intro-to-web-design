b734d74d12051fd81aba252420a1c0e1f9416a7a
notsolegendaryshan <s.farquharson001@gmail.com>
2024-03-18 23:20:05 -0500

    JavaScript/jQuery Update

--- scripts/site.js
+++ scripts/site.js
@@ -1,3 +1,15 @@
 function ClearForm(){
     document.getElementById("contactForm").reset();
 }
+
+function hidePTags(){
+    let pTags = document.getElementsByTagName('p');
+    for(let item of pTags){
+        // item.style.visibility = 'hidden';
+        item.style.display = 'none';
+    }
+}
+
+$('#jqueryBtn').click(function(){
+    $('p').hide();
+})
